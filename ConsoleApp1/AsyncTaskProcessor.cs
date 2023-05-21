8f78292557be1687568688b7c0715a234a5e42e0
Сергей <muromtsev1996@gmail.com>
2023-05-21 11:47:35 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/AsyncTaskProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class AsyncTaskProcessor
+    {
+        private readonly Queue<Func<Task>> _taskQueue = new Queue<Func<Task>>();
+        private bool _isProcessing = false;
+
+        public void AddTask(Func<Task> task)
+        {
+            _taskQueue.Enqueue(task);
+            if (!_isProcessing)
+            {
+                ProcessTasks();
+            }
+        }
+
+        private async void ProcessTasks()
+        {
+            _isProcessing = true;
+            while (_taskQueue.Count > 0)
+            {
+                var task = _taskQueue.Dequeue();
+                await task();
+            }
+            _isProcessing = false;
+        }
+
+        public async Task WaitAllTasks()
+        {
+            while (_isProcessing)
+            {
+                await Task.Delay(10);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+IEnumerable<double> GetRowSums(IEnumerable<double> row)
+{
+    double sum = 0;
+    return row.Select(x => sum += x);
+}
+List<double> d = new List<double>() { 1, 2, 3, 4 };
+var s = GetRowSums(d);
+foreach(var k in s)
+{
+    Console.WriteLine(k);
+}
+
+AsyncTaskProcessor taskProcessor = new AsyncTaskProcessor();
+taskProcessor.AddTask(async () =>
+{
+    Console.WriteLine("Task 1 started");
+    await Task.Delay(1000);
+    Console.WriteLine("Task 1 finished");
+});
+
+taskProcessor.AddTask(async () =>
+{
+    Console.WriteLine("Task 2 started");
+    await Task.Delay(2000);
+    Console.WriteLine("Task 2 finished");
+});
+
+await taskProcessor.WaitAllTasks();
+Console.WriteLine("All tasks finished");
+
